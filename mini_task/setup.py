24d0ed152ef74687545109aadc2a02f1046cc905
Ahmad Abouelainein <ahmad.abouelainein@gmail.com>
2025-03-27 12:55:35 +0200

    create custom ros2 test for p2p controller and cleanup

--- mini_task/setup.py
+++ mini_task/setup.py
@@ -9,7 +9,7 @@ setup(
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
-        ['reso  urce/' + package_name]),
+        ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
    	(os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
    	(os.path.join('share', package_name, 'model'), glob('model/*')),
